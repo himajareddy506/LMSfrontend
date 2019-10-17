b1e656f2ce7ec2adcb775a74353c54e1e086aeac
himajareddy506 <himajareddy.cse@gmail.com>
2019-10-17 15:06:38 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   // baseUrl: 'http://10.117.189.134:8082',
-  baseUrl: 'http://35.154.199.225:8888'
+  baseUrl: 'http://35.154.199.225:8082'
 };
 
 /*
